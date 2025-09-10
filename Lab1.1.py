abf87b3c5012637ac33a13858a3fa5f451eced4d
Valentyn <valentynbuga90@gmail.com>
2025-09-10 10:37:52 +0300

    Create Lab1.1.py

---
+++ Lab1.1.py
@@ -0,0 +1,21 @@
+name = "valik"                   
+age = 16                            
+group = "IST_22"                   
+colors = "black_blue"              
+games = "RDR2, NFS_Underground"   
+height = 180                         
+km = 20.8                           
+student = True                       
+adult = False                         
+shopping = "snacks"                   
+
+print(name, type(name))
+print(age, type(age))
+print(group, type(group))
+print(colors, type(colors))
+print(games, type(games))
+print(height, type(height))
+print(km, type(km))
+print(student, type(student))
+print(adult, type(adult))
+print(shopping, type(shopping))
